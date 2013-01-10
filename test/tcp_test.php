cf3f6c016750001524d867856307b3af776ae127
Ilya Shpigor <petrsum@gmail.com>
2013-01-10 12:31:06 +0400

    Implement the tcp_test

--- test/tcp_test.php
+++ test/tcp_test.php
@@ -1,13 +1,15 @@
 #!/usr/bin/env php
 
-import registrar_client
+<?
+include("registrar_client.php");
 
-client = registrar_client.RegistrarClient("tcp_test.log", "tcp://localhost:5570", 0)
+$client = new RegistrarClient("tcp_test.log", "tcp://localhost:5570", 0);
 
-key = "key1"
-data = "test data 1\0"
+$key = "key1";
+$data = "test data 1\0";
 
-client.WriteData(key, data, len(data))
-result = client.ReadData(key) + '\0'
+$client->WriteData($key, $data, strlen($data));
+$result = $client->ReadData($key)."\0";
 
-assert data == result
+assert($data === $result);
+?>
