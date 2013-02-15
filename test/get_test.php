7086d015ebee7442c5b4983bd0c79cc59e3edaed
Ilya Shpigor <petrsum@gmail.com>
2013-02-15 14:41:19 +0400

    Use ClientWrap class in the get_test.php script

--- test/get_test.php
+++ test/get_test.php
@@ -1,15 +1,33 @@
 #!/usr/bin/env php
 
 <?php
-include("registrar_client.php");
+include("client_wrap.php");
 
-$client = new RegistrarClient("get_test.log", "ipc:///var/run/zero-cache/0", 0);
+define('KEY_LONG', 'key1');
+define('DATA_LONG', 1024);
 
-$key = "key1";
-$data = "test data 1\0";
+define('KEY_DOUBLE', 'key2');
+define('DATA_DOUBLE', 100.53);
 
-$client->WriteData($key, $data, strlen($data));
-$result = $client->ReadData($key)."\0";
+define('KEY_STRING', 'key3');
+define('DATA_STRING', 'test data');
 
-assert($data === $result);
-?>
+function init_data($client)
+{
+    $client->WriteLong(KEY_LONG, DATA_LONG);
+    $client->WriteDouble(KEY_DOUBLE, DATA_DOUBLE);
+    $client->WriteString(KEY_STRING, DATA_STRING);
+}
+
+function check_data($client)
+{
+    assert(DATA_LONG == $client->ReadLong(KEY_LONG));
+    assert(DATA_DOUBLE == $client->ReadDouble(KEY_DOUBLE));
+    assert(DATA_STRING == $client->ReadString(KEY_STRING));
+}
+
+$client = new ClientWrap("get_test.log", "ipc:///var/run/zero-cache/0", 0);
+
+init_data($client);
+
+check_data($client);
